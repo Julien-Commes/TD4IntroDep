36c8caff48eb6ccc07f258787c37f8d6ed98f7d1
felixjosquin <fj.josquin@gmail.com>
2021-06-05 13:05:49 +0200

    prgm list

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ char* copy (char* s){
     return res;
 }
 
-void print_l (char* s){
+void print_char (char* s){
     printf("%c",*s);
     while(*s++){
         printf("%c",*s);
@@ -36,8 +36,32 @@ void print_l (char* s){
     printf("\n");
 }
 
+
+int* list_empty(){
+    int* r=malloc(sizeof(int));
+    *r=0;
+    return r;
+}
+
+void append (int* s,int v){
+    int n=*s;
+    *s=*s+1;
+    *(s+n+1)=v;
+}
+
+void print_list(int* s){
+    int n=*s;
+    printf("[");
+    for (int i=1;i<n;i++){
+        printf(" %i ,",*(s+i));
+    }
+    printf(" %i ]\n",*(s+n));
+}
+
 int main(){
-    char* txt="abeefoirec";
-    char* txt_copy= copy(txt);
-    print_l(txt_copy);
+    int* l=list_empty();
+    append(l,5);
+    append(l,8);
+    append(l,6);
+    print_list(l);
 }
